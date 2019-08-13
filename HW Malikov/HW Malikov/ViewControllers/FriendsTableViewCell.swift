d51a51fe41a7e5305f186ac208250f8e530e9d51
Andrey Malikov <a.malikov.int@gmail.com>
2019-08-13 23:59:04 +0300

    Добавлен вывод списка друзей

---
+++ HW Malikov/HW Malikov/ViewControllers/FriendsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FriendsTableViewCell.swift
+//  HW Malikov
+//
+//  Created by Andrey Malikov on 13/08/2019.
+//  Copyright © 2019 Andrey Malikov. All rights reserved.
+//
+
+import UIKit
+
+class FriendsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
