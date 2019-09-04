361d1e7fe63fdc0273a03e4f13a4a3db53844faa
Andrey Malikov <a.malikov.int@gmail.com>
2019-09-04 23:51:45 +0300

    добавлен Layout для коллекции фото

---
+++ HW Malikov/HW Malikov/ViewControllers/FriendsCollection/FriendsCollectionVC+ UICollectionViewDelegateFlowLayout.swift
@@ -0,0 +1,11 @@
+//
+//  FriendsCollectionVC+.swift
+//  HW Malikov
+//
+//  Created by Andrey Malikov on 04/09/2019.
+//  Copyright © 2019 Andrey Malikov. All rights reserved.
+//
+
+import UIKit
+
+
